5fe88163baa84fbd29054f6c89ef1b07c51ff226
gizemcoban <gizem.coban.r@gmail.com>
2025-06-09 14:32:37 +0200

    learnedwords component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Wordsave from './components/Wordsave'
 import AppRoutes from './routes/AppRoutes'
+import LearnedWords from './components/LearnedWords'
 function App() {
 
   return (
     <>
-      <Wordsave/>
+      <LearnedWords/>
       {/**<AppRoutes /> */}
     </>
   )
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,16 @@
 import {lazy, Suspense} from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import LearnedWords from '../components/LearnedWords';
 
 export default function AppRoutes() {
   const Wordsave = lazy(()=>import('../components/Wordsave'))
+  const LearnedWords = lazy(()=>import('../components/LearnedWords'))
   return (
     <>
       <Suspense fallback={null}>
         <Routes>
             <Route path="/*" element={<Wordsave />}></Route>
+            <Route path="/learnedWords" element={<LearnedWords />}></Route>
         </Routes>
       </Suspense>
     </>
