0b2557822666d67cad1de2c4ab02f1d9d42f2461
gizemcoban <gizem.coban.r@gmail.com>
2025-06-17 15:22:27 +0200

    training style

--- src/pages/Training.jsx
+++ src/pages/Training.jsx
@@ -6,26 +6,29 @@ export default function Training() {
   const [isModalOpen,setIsModalOpen]=useState(false);
   return (
     <>
-      <div className="grid grid-cols-2 gap-4 p-12 bg-[#FCFCFC] rounded-3xl">
-        <div className="flex flex-col items-start justify-between">
-          <div className="flex flex-row justify-between w-[400px] h-[200px]">
-            <input className=""></input>
+      <div className="grid grid-cols-2 gap-0 p-5 bg-[#FCFCFC] rounded-3xl">
+        <div className="flex flex-col items-start justify-center gap-0 bg-[#FCFCFC]">
+          <div className="flex flex-row justify-between w-[400px] h-[200px] border  border-white border-r-[#DBDBDB] p-5">
+            <input className="" placeholder="UK"></input>
             <div className="flex flex-row items-start justify-around gap-2 p-4">
               <img src={ukrainian} alt="ukrainian" />
               <p>Ukrainian</p>
             </div>
+                      
+
           </div>
-          <button>Next</button>
+           <button className="flex text-[#121417]/50">Next</button>
+          
         </div>
-          <div className="flex flex-row justify-between w-[400px] h-[200px] ">
-          <input className=""></input>
+          <div className="flex flex-row justify-between w-[400px] h-[200px] border border-white border-l-[#DBDBDB] p-5">
+          <input className="" placeholder="EN"></input>
             <div className="flex flex-row items-start justify-around gap-2 p-4">
               <img src={english} alt="english" />
               <p>English</p>
             </div>
         </div>
       </div>
-      <div className="flex flex-row items-center gap-10">
+      <div className="flex flex-row items-center gap-10 mt-10">
         <button className="colorfulButton" onClick={()=>setIsModalOpen(true)}>Save</button>
         <button className="colorfulButton">Cancel</button>
       </div>
