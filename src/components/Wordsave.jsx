a7d856a81ad0548d9a2b83d933da5956288d8d81
gizemcoban <gizem.coban.r@gmail.com>
2025-06-17 19:16:25 +0200

    dictionary delete function

--- src/components/Wordsave.jsx
+++ src/components/Wordsave.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
-import { createWord } from "../redux/vocabs/vocabOps.js";
+import {  editWord } from "../redux/vocabs/vocabOps.js";
 import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
-import ukrainian from '../../src/assets/ukraine.svg';
-import english from '../../src/assets/english.svg';
+import ukrainian from "../../src/assets/ukraine.svg";
+import english from "../../src/assets/english.svg";
 import { createPortal } from "react-dom";
 
-export default function Wordsave({closeModal}) {
+export default function Wordsave({ id,payload,closeModal }) {
   const dispatch = useDispatch();
 
   const validationForm = Yup.object().shape({
@@ -23,7 +23,7 @@ export default function Wordsave({closeModal}) {
   const handleSubmit = async (values, { resetForm }) => {
     const { word1, word2 } = values;
     try {
-      await dispatch(createWord({ en:word1, ua:word2 })).unwrap();
+      await dispatch(editWord({ en: word1, ua: word2 ,category, isRegular})).unwrap();
       resetForm();
       toast.success("Word is added.");
     } catch (error) {
@@ -33,80 +33,90 @@ export default function Wordsave({closeModal}) {
   };
 
   return createPortal(
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/70 backdrop-blur-sm">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/70 backdrop-blur-sm">
+      <div className="bg-[#85AA9F] rounded-3xl p-12 absolute">
+        <button
+          onClick={closeModal}
+          className="text-white absolute top-3 right-6"
+        >
+          X
+        </button>
 
-    <div className="bg-[#85AA9F] rounded-3xl p-12">
-      <Formik
-        initialValues={{ word1: "", word2: "" }}
-        validationSchema={validationForm}
-        onSubmit={handleSubmit}
-      >
-        {({ isSubmitting, resetForm }) => (
-          <Form>
-            <div className="flex flex-col gap-4">
-              <button onClick={closeModal}>X</button>
-              <div className="mb-2 flex flex-col gap-5">
-                <div className="flex flex-row items-start">
-                  <Field
-                    name="word1"
-                    type="text"
-                    className="w-65 text-white border border-white/40 rounded-xl p-2"
-                    placeholder="Word"
-                  />
-                  <div className="rounded-xl ml-2 mt-2 flex flex-row gap-2">
-                    <img src={ukrainian} alt="ukrainian" className="h-7 w-7" />
-                    <p className="text-white">Ukrainian</p>
+        <Formik
+          initialValues={{ word1: "", word2: "" }}
+          validationSchema={validationForm}
+          onSubmit={handleSubmit}
+        >
+          {({ isSubmitting, resetForm }) => (
+            <Form>
+              <div className="flex flex-col gap-4 ">
+                <div className="mb-2 flex flex-col gap-5">
+                  <div className="flex flex-row items-start">
+                    <Field
+                      name="word1"
+                      type="text"
+                      className="w-65 text-white border border-white/40 rounded-xl p-2"
+                      placeholder="Word"
+                    />
+                    <div className="rounded-xl ml-2 mt-2 flex flex-row gap-2">
+                      <img
+                        src={ukrainian}
+                        alt="ukrainian"
+                        className="h-7 w-7"
+                      />
+                      <p className="text-white">Ukrainian</p>
+                    </div>
                   </div>
-                </div>
-                <ErrorMessage
-                  name="word1"
-                  component="div"
-                  className="text-red-600 text-sm"
-                />
-              </div>
-
-              <div className="mb-2">
-                <div className="flex flex-row items-start">
-                  <Field
+                  <ErrorMessage
                     name="word1"
-                    type="text"
-                    className="w-65 text-white border border-white/40 rounded-xl p-2"
-                    placeholder="Word"
+                    component="div"
+                    className="text-red-600 text-sm"
                   />
-                  <div className="rounded-xl ml-2 mt-2 flex flex-row gap-2">
-                    <img src={english} alt="english" className="h-7 w-7" />
-                    <p className="text-white">English</p>
-                  </div>
                 </div>
-                <ErrorMessage
-                  name="word2"
-                  component="div"
-                  className="text-red-600 text-sm"
-                />
-              </div>
 
-              <div className="flex flex-row justify-around gap-4">
-                <button
-                  type="submit"
-                  className="border border-white/40 rounded-2xl w-full p-2 bg-white font-semibold"
-                  disabled={isSubmitting}
-                >
-                  {isSubmitting ? "Saving..." : "Save"}
-                </button>
+                <div className="mb-2">
+                  <div className="flex flex-row items-start">
+                    <Field
+                      name="word2"
+                      type="text"
+                      className="w-65 text-white border border-white/40 rounded-xl p-2"
+                      placeholder="Word"
+                    />
+                    <div className="rounded-xl ml-2 mt-2 flex flex-row gap-2">
+                      <img src={english} alt="english" className="h-7 w-7" />
+                      <p className="text-white">English</p>
+                    </div>
+                  </div>
+                  <ErrorMessage
+                    name="word2"
+                    component="div"
+                    className="text-red-600 text-sm"
+                  />
+                </div>
 
-                <button
-                  type="button"
-                  onClick={() => resetForm()}
-                  className="border border-white/40 rounded-2xl w-full p-2 text-white font-semibold"
-                >
-                  Cancel
-                </button>
+                <div className="flex flex-row justify-around gap-4">
+                  <button
+                    type="submit"
+                    className="border border-white/40 rounded-2xl w-full p-2 bg-white font-semibold"
+                    disabled={isSubmitting}
+                  >
+                    {isSubmitting ? "Saving..." : "Save"}
+                  </button>
+
+                  <button
+                    type="button"
+                    onClick={() => resetForm()}
+                    className="border border-white/40 rounded-2xl w-full p-2 text-white font-semibold"
+                  >
+                    Cancel
+                  </button>
+                </div>
               </div>
-            </div>
-          </Form>
-        )}
-      </Formik>
-    </div>
-    </div>, document.body
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>,
+    document.body
   );
 }
--- src/pages/Dictionary.jsx
+++ src/pages/Dictionary.jsx
@@ -1,113 +1,185 @@
 import { BsSearch } from "react-icons/bs";
 import english from "../assets/english.svg";
 import ukrainian from "../assets/ukraine.svg";
-import { FaArrowRight } from "react-icons/fa6";
 import { useEffect, useState } from "react";
 import Wordsave from "../components/Wordsave";
 import Welldone from "../components/Welldone";
-import {getWords} from '../redux/vocabs/vocabOps'
-import {useDispatch} from "react-redux"
+import { getWords, deleteWord } from "../redux/vocabs/vocabOps";
+import { useDispatch } from "react-redux";
+
 export default function Dictionary() {
   const [selectedWordType, setSelectedWordType] = useState("verb");
-  const [isModalOpen, setIsModalOpen]=useState(false);
-  const [words,setWords]=useState([]);
-  const [isEdit,setIsEdit]=useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [words, setWords] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const dispatch = useDispatch();
+  const [selectedWord, setSelectedWord] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+
   const handleSelect = (e) => {
     setSelectedWordType(e.target.value);
   };
 
-  useEffect(()=>{
-   const fetchWords = async()=>{
-    const words = await dispatch(getWords()).unwrap();
-    setWords(words);
-    return words;
-   }
-   fetchWords();
-  },[]);
+  useEffect(() => {
+    const fetchWords = async () => {
+      const words = await dispatch(getWords()).unwrap();
+      setWords(words);
+    };
+    fetchWords();
+  }, [dispatch]);
 
+  const handleDelete = async(id) => {
+    await dispatch(deleteWord(id)); // redux store'dan da silinir
+  const updatedWords = await dispatch(getWords()).unwrap(); // tekrar fetch et
+    setSelectedWord(null);
+    setIsEdit(false);
+      setWords(updatedWords);
 
+  };
 
   return (
-<>
-    <div>
-      <div className="flex flex-row items-center justify-between w-300">
-        <div className="flex flex-row rounded-3xl w-150 p-12 items-center gap-3">
-          <div className="flex flex-row items-center border border-[#121417]/10 rounded-lg p-1">
-            <input type="text" placeholder="Find the word " className="w-36" />
-            <BsSearch />
+    <>
+      <div>
+        <div className="flex flex-row items-center justify-between w-300">
+          <div className="flex flex-row rounded-3xl w-150 p-12 items-center gap-3">
+            <div className="flex flex-row items-center border border-[#121417]/10 rounded-lg p-1">
+              <input
+                type="text"
+                placeholder="Find the word "
+                className="w-36"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+              <BsSearch />
+            </div>
+            <select
+              name="wordType"
+              id="wordType"
+              onChange={handleSelect}
+              value={selectedWordType}
+            >
+              <option value="verb">Verb</option>
+              <option value="participle">Participle</option>
+              <option value="noun">Noun</option>
+              <option value="adjective">Adjective</option>
+              <option value="pronoun">Pronoun</option>
+              <option value="numerals">Numerals</option>
+              <option value="adverb">Adverb</option>
+              <option value="preposition">Preposition</option>
+              <option value="conjunction">Conjunction</option>
+              <option value="phrasalVerb">Phrasal verb</option>
+              <option value="functionalPhrase">Functional phrase</option>
+            </select>
           </div>
-          <select name="wordType" id="wordType" onChange={handleSelect} value={selectedWordType} >
-            <option value="verb">Verb</option>
-            <option value="participle">Participle</option>
-            <option value="noun">Noun</option>
-            <option value="adjective">Adjective</option>
-            <option value="pronoun">Pronoun</option>
-            <option value="numerals">Numerals</option>
-            <option value="adverb">Adverb</option>
-            <option value="preposition">Preposition</option>
-            <option value="conjunction">Conjunction</option>
-            <option value="phrasalVerb">Phrasal verb</option>
-            <option value="functionalPhrase">Functional phrase</option>
-          </select>
+          <div className="flex flex-row gap-4">
+            <p>To study: </p>
+            <button
+              onClick={() => setIsModalOpen(true)}
+              className="font-semibold"
+            >
+              Add Word +
+            </button>
+            <p className="font-semibold">Train oneself </p>
+          </div>
         </div>
-        <div className="flex flex-row gap-4">
-          <p>To study: </p>
-          <button onClick={()=>setIsModalOpen(true)} className="font-semibold">Add Word +</button>
-          <p className="font-semibold">Train oneself </p>
+
+        <div className="flex flex-row items-center justify-between w-300">
+          <table className="min-w-full border border-gray-300">
+            <thead>
+              <tr className="bg-[#85AA9F]/10">
+                <th className="border border-gray-300 px-4 py-2 text-left">
+                  <div className="flex flex-row justify-between items-center gap-2">
+                    <p>Word</p>
+                    <img src={english} alt="english" />
+                  </div>
+                </th>
+                <th className="border border-gray-300 px-4 py-2 text-left">
+                  <div className="flex flex-row justify-between items-center gap-2">
+                    <p>Translation</p>
+                    <img src={ukrainian} alt="ukrainian" />
+                  </div>
+                </th>
+                <th className="border border-gray-300 px-4 py-2 text-left">
+                  Category
+                </th>
+                <th className="border border-gray-300 px-4 py-2 text-left">
+                  Progress
+                </th>
+                <th className="border border-gray-300 px-4 py-2 text-left"></th>
+              </tr>
+            </thead>
+            <tbody className="text-left">
+              {words
+                .filter((word) =>
+                  word.en.toLowerCase().includes(searchTerm.toLowerCase())
+                )
+                .map((word) => (
+                  <tr key={word.id}>
+                    <td className="border border-gray-300 px-4 py-2">
+                      {word.en}
+                    </td>
+                    <td className="border border-gray-300 px-4 py-2">
+                      {word.ua}
+                    </td>
+                    <td className="border border-gray-300 px-4 py-2">
+                      {word.category}
+                    </td>
+                    <td className="border border-gray-300 px-4 py-2">
+                      progress
+                    </td>
+                    <td className="border border-gray-300 p-2 text-center">
+                      <button
+                        onClick={() => {
+                          setSelectedWord(word);
+                          setIsEdit(true);
+                        }}
+                      >
+                        ...
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
         </div>
       </div>
-            <div className="flex flex-row items-center justify-between w-300">
-              <table className="min-w-full border border-gray-300">
-                <thead>
-                  <tr className="bg-[#85AA9F]/10">
-                    <th className="border border-gray-300 px-4 py-2 text-left">
-                      <div className="flex flex-row justify-between items-center gap-2">
-                        <p>Word</p>
-                        <img src={english} alt="english" />
-                      </div>
-                    </th>
-                    <th className="border border-gray-300 px-4 py-2 text-left">
-                      <div className="flex flex-row justify-between items-center gap-2">
-                        <p>Translation</p>
-                        <img src={ukrainian} alt="ukrainian" />
-                      </div>
-                    </th>
-                    <th className="border border-gray-300 px-4 py-2 text-left">
-                      Category
-                    </th>
-                    <th className="border border-gray-300 px-4 py-2 text-left">
-                      Progress
-                    </th>
-                    <th className="border border-gray-300 px-4 py-2 text-left">
-                         
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="text-left">
-              {words.map((word) => (
-                <tr key={word.id}>
-                  <td className="border border-gray-300 px-4 py-2">{word.en}</td>
-                  <td className="border border-gray-300 px-4 py-2">{word.ua}</td>
-                  <td className="border border-gray-300 px-4 py-2">{word.category}</td>
-                  <td className="border border-gray-300 px-4 py-2">
-                    progress
-                  </td>
-                  <td className="border border-gray-300 flex justify-center items-center p-2">
-                  <button onClick={()=>setIsEdit(!isEdit)} >...</button>
-                  {isModalOpen && (<Welldone closeModal={()=>setIsModalOpen(false)} />)}
-                  </td>
-                </tr>
-              ))}
-                </tbody>
-              </table>
-            </div>
-    </div>
-    {isEdit && (<div className="w-full flex flex-col p-3 gap-2">
-      <button>Edit</button>
-      <button>Delete</button>
-    </div>)}
-    
-</>
+
+      {isModalOpen && (
+        <Welldone closeModal={() => setIsModalOpen(false)} />
+      )}
+
+      {selectedWord && isEdit && (
+        <div className="w-full flex flex-col p-3 gap-2">
+          <button
+            type="button"
+            onClick={() => setIsEditModalOpen(true)}
+            className="text-blue-600"
+          >
+            Edit
+          </button>
+          <button
+            type="button"
+            onClick={() => handleDelete(selectedWord.id)}
+            className="text-red-600"
+          >
+            Delete
+          </button>
+
+        </div>
+      )}
+
+      {isEditModalOpen && selectedWord && (
+        <Wordsave
+          category={selectedWord.category}
+          isIrregular={selectedWord.isRegular}
+          closeModal={() => {
+            setIsEditModalOpen(false);
+            setIsEdit(false);
+            setSelectedWord(null);
+          }}
+        />
+      )}
+    </>
   );
 }
