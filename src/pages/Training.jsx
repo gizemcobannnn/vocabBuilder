09d768425671d0be689e3f7eb606d76a704712d3
gizemcoban <gizem.coban.r@gmail.com>
2025-06-20 08:01:16 +0200

    CountdownCircleTimer

--- src/pages/Training.jsx
+++ src/pages/Training.jsx
@@ -3,65 +3,100 @@ import english from "../assets/english.svg";
 import { useState } from "react";
 import Welldone from "../components/Welldone";
 import { useEffect } from "react";
-import { getTasks,createAnswer } from "../redux/vocabs/vocabOps";
+import { getTasks, createAnswer } from "../redux/vocabs/vocabOps";
 import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
-import generateObjectId from "../utils/generateObjectId"
+import generateObjectId from "../utils/generateObjectId";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+
 export default function Training() {
-  const [isModalOpen,setIsModalOpen]=useState(false);
-  const dispatch= useDispatch();
-  const [ua,setua]=useState("");
-  const [en,seten]=useState("")
-  const [tasks,setTasks]=useState([])
-  const id= generateObjectId();
-    const handleSave=async()=>{
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const dispatch = useDispatch();
+  const [ua, setua] = useState("");
+  const [en, seten] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const id = generateObjectId();
+  const handleSave = async () => {
     setIsModalOpen(true);
-    const answerData={_id:id.toString(),en,ua,task:"en"}
-     await dispatch(createAnswer(answerData)).unwrap();
-    setTasks(prev=>[...prev,answerData]);
-  }
-  useEffect(()=>{
-    const getTask=async()=>{
-      try{
-        const tasksResponse=await dispatch(getTasks()).unwrap();
+    const answerData = { _id: id.toString(), en, ua, task: "en" };
+    await dispatch(createAnswer(answerData)).unwrap();
+    setTasks((prev) => [...prev, answerData]);
+  };
+  useEffect(() => {
+    const getTask = async () => {
+      try {
+        const tasksResponse = await dispatch(getTasks()).unwrap();
         setTasks(tasksResponse.words);
-      }catch(e){
-        toast.error("tasks did not fetched"+e)
+      } catch (e) {
+        toast.error("tasks did not fetched" + e);
       }
-    }
+    };
     getTask();
-    
-  })
+  });
   return (
     <>
-      <div className="grid grid-cols-2 gap-0 p-5 bg-[#FCFCFC] rounded-3xl">
-        <div className="flex flex-col items-start justify-center gap-0 bg-[#FCFCFC]">
-          <div className="flex flex-row justify-between items-start w-[400px] h-[200px] border  border-white border-r-[#DBDBDB] p-6">
-            <input className="inputword" placeholder="UK" value={ua} onChange={(e)=>setua(e.target.value)}></input>
-            <div className="flex flex-row items-start justify-around gap-2">
-              <img src={ukrainian} alt="ukrainian" />
-              <p>Ukrainian</p>
-            </div>
-                      
-
-          </div>
-           <button className="flex text-[#121417]/50">Next</button>
-          
+      <div className="flex flex-col">
+        <div className="flex flex-row self-end h-5 w-10 m-20">
+          <CountdownCircleTimer
+            isPlaying
+            duration={25}
+            colors={["#85AA9F"]}
+            size={75}
+            strokeWidth={7}
+            onComplete={() => {
+              return { shouldRepeat: true, delay: 1 };
+            }}
+          >
+            {({ remainingTime }) => (
+              <div className="flex items-center justify-center h-full w-full text-lg text-gray-800">
+                {remainingTime}
+              </div>
+            )}
+          </CountdownCircleTimer>
         </div>
+
+        <div className="grid grid-cols-2 gap-0 p-5 bg-[#FCFCFC] rounded-3xl">
+          <div className="flex flex-col items-start justify-center gap-0 bg-[#FCFCFC]">
+            <div className="flex flex-row justify-between items-start w-[400px] h-[200px] border  border-white border-r-[#DBDBDB] p-6">
+              <input
+                className="inputword"
+                placeholder="UK"
+                value={ua}
+                onChange={(e) => setua(e.target.value)}
+              ></input>
+              <div className="flex flex-row items-start justify-around gap-2">
+                <img src={ukrainian} alt="ukrainian" />
+                <p>Ukrainian</p>
+              </div>
+            </div>
+            <button className="flex text-[#121417]/50">Next</button>
+          </div>
           <div className="flex flex-row justify-between items-start w-[400px] h-[200px] border border-white border-l-[#DBDBDB] p-6">
-          <input className="inputword" placeholder="EN" value={en} onChange={(e)=>seten(e.target.value)}></input>
+            <input
+              className="inputword"
+              placeholder="EN"
+              value={en}
+              onChange={(e) => seten(e.target.value)}
+            ></input>
             <div className="flex flex-row items-start justify-around gap-2">
               <img src={english} alt="english" />
               <p>English</p>
             </div>
+          </div>
+        </div>
+        <div className="flex flex-row items-center gap-10 mt-10">
+          <button
+            className="colorfulButton"
+            onClick={handleSave}
+            disabled={ua === "" || en === ""}
+          >
+            Save
+          </button>
+          <button className="colorfulButton">Cancel</button>
         </div>
       </div>
-      <div className="flex flex-row items-center gap-10 mt-10">
-        <button className="colorfulButton" onClick={handleSave} disabled={ua === "" || en === ""}>Save</button>
-        <button className="colorfulButton">Cancel</button>
-      </div>
       {isModalOpen && (
-        <Welldone closeModal={()=>setIsModalOpen(false)} tasks={tasks}/>
+        <Welldone closeModal={() => setIsModalOpen(false)} tasks={tasks} />
       )}
     </>
   );
